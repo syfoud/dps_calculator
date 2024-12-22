ae12d4090cce3d2be27dcdfe11664dbd7189eaad
syfoud <xing12130@outlook.com>
2024-12-22 18:49:03 +0800

    大致完善前端

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QListWidgetItem
 import sys
 
 class MyApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('dps_calculator.ui', self)
+
+        # 添加一些项
+        for i in range(1,22):
+            item = QListWidgetItem(f"card {i}")
+            self.cardlist.addItem(item)
+
+            # 设置每个项的大小
+            item.setSizeHint(QSize(50, 100))
 
     def sayHi(self):  # 槽函数
         print("hi")
