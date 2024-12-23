9bbcf3feafd73f23aa9096b4d3923e032300a587
syfoud <xing12130@outlook.com>
2024-12-23 16:11:26 +0800

    完全实现障碍与人物在地图上的添加

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt6 import QtWidgets, uic
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QIcon, QPixmap, QCursor, QPalette, QBrush
-from PyQt6.QtWidgets import QApplication, QListWidgetItem, QFileDialog
+from PyQt6.QtWidgets import QApplication, QListWidgetItem, QFileDialog, QLabel, QSizePolicy
 import sys
 import os
 
@@ -25,7 +25,9 @@ class MyApp(QtWidgets.QMainWindow):
         self.set_table_background('picture/map/美味岛/色拉岛（陆）.jpg', self.battle_ground, (368, 85, 670, 600),22)
 
         # 初始化鼠标样式状态
-        self.shovel_active = "cursor"
+        self.mouse_active = "cursor"
+        # 记忆化人物位置
+        self.current_people_position = None
         # 连接槽函数
         self.shovel.clicked.connect(self.toggle_shovel_cursor)
         self.obstacle.clicked.connect(self.toggle_obstacle_cursor)
@@ -33,6 +35,48 @@ class MyApp(QtWidgets.QMainWindow):
         self.map_change.clicked.connect(self.set_background_image)
 
         self.close_map.clicked.connect(self.clear_table_background)
+
+        self.battle_ground.cellClicked.connect(self.on_cell_clicked)
+
+    def on_cell_clicked(self, row, column):
+        if self.mouse_active == "shovel":
+            # 清除单元格中的图片
+            self.battle_ground.setCellWidget(row, column, None)
+        else:
+            image_path = ''
+            if self.mouse_active == "obstacle":
+                image_path = 'picture/ui/飞行路障鼠【特殊-路障】.png'
+            elif self.mouse_active == "people":
+                self.remove_previous_image(self.current_people_position)
+                self.current_people_position = (row, column)
+                image_path = 'picture/ui/章鱼小丸子.png'
+            else:
+                print("Unknown mouse_active state")
+                return
+
+            self.add_image_to_cell(image_path, row, column)
+
+    def remove_previous_image(self, position):
+        if position is not None:
+            row, column = position
+            self.battle_ground.setCellWidget(row, column, None)
+
+    def add_image_to_cell(self, image_path, row, column):
+        label = QLabel()
+        pixmap = QPixmap(image_path)
+        if not pixmap.isNull():
+            # 使用固定尺寸调整图像大小
+            fixed_size = QSize(60, 60)  # 设置固定尺寸
+            scaled_pixmap = pixmap.scaled(fixed_size, Qt.AspectRatioMode.KeepAspectRatio,
+                                          Qt.TransformationMode.SmoothTransformation)
+            label.setPixmap(scaled_pixmap)
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+            # 将 QLabel 添加到单元格中
+            self.battle_ground.setCellWidget(row, column, label)
+        else:
+            print(f"Failed to load image: {self.image_path}")
 
     def set_background_image(self):
         default_dir = os.path.join(os.path.dirname(__file__), 'picture', 'map')
@@ -65,32 +109,32 @@ class MyApp(QtWidgets.QMainWindow):
 
 
     def toggle_shovel_cursor(self):
-        if self.shovel_active!="shovel":
+        if self.mouse_active!= "shovel":
             pixmap = QPixmap('picture/ui/shovel.png')
             cursor = QCursor(pixmap)
             self.setCursor(cursor)
-            self.shovel_active = "shovel"
+            self.mouse_active = "shovel"
         else:
             self.unsetCursor()
-            self.shovel_active = "cursor"
+            self.mouse_active = "cursor"
     def toggle_obstacle_cursor(self):
-        if self.shovel_active!="obstacle":
+        if self.mouse_active!= "obstacle":
             pixmap = QPixmap('picture/ui/飞行路障鼠【特殊-路障】.png')
             cursor = QCursor(pixmap)
             self.setCursor(cursor)
-            self.shovel_active = "obstacle"
+            self.mouse_active = "obstacle"
         else:
             self.unsetCursor()
-            self.shovel_active = "cursor"
+            self.mouse_active = "cursor"
     def toggle_people_cursor(self):
-        if self.shovel_active!="people":
+        if self.mouse_active!= "people":
             pixmap = QPixmap('picture/ui/章鱼小丸子.png')
             cursor = QCursor(pixmap)
             self.setCursor(cursor)
-            self.shovel_active = "people"
+            self.mouse_active = "people"
         else:
             self.unsetCursor()
-            self.shovel_active = "cursor"
+            self.mouse_active = "cursor"
 
 
 
